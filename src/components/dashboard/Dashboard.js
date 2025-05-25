567a9f0b175df5fed367d9f303705c1414e4c1c7
Oskar Gester <gester.oskar@gmail.com>
2025-05-25 20:13:08 +0200

    next step, figure out structure and prepare for responsiveness

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -2,12 +2,14 @@ import styles from "../../styles/layout/dashboardLayout.module.scss";
 
 export default async function Dashboard() {
   return (
-    <div className={styles.dashboardContainer}>
-      <div className={styles.topContainer}>tobar here</div>
-      <div className={styles.leftContainer}>sidebar here</div>
+    <div className={styles.grid}>
+      <div className={styles.topContainer}>top-bar here</div>
+      <div className={styles.leftContainer}>side-bar here</div>
       <div className={styles.mainContainer}>main section here</div>
       <div className={styles.statsContainer}>stats section here</div>
       <div className={styles.bottomContainer}>bottom here</div>
     </div>
   );
 }
+
+//<div className={styles.dashboardContainer}></div>
