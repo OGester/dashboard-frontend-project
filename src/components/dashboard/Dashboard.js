1115b669f40c385bbe09a52fee1687f13cf1fe49
Oskar Gester <gester.oskar@gmail.com>
2025-06-06 09:47:27 +0200

    rearange push

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -7,21 +7,12 @@ export default async function Dashboard() {
   return (
     <div className={styles.wrapper}>
       <div className={styles.grid}>
-        <div className={styles.topContainer}>
-          <p>My Name here</p>
-        </div>
-        <div className={styles.leftContainer}>
-          <p>Side stuff here</p>
-        </div>
-        <div className={styles.mainContainer}>
-          <p>About me here</p>
-        </div>
-        <div className={styles.statsContainer}>
-          <p>stats section here</p>
-        </div>
-        <div className={styles.bottomContainer}>
-          <p>bottom here</p>
-        </div>
+        <div className={styles.header}>Header here</div>
+        <div className={styles.sidebar}>Sidebar here</div>
+        <div className={styles.about_box}>About me here</div>
+        <div className={styles.stats_box}>Stats section here</div>
+        <div className={styles.bottom_box}>Bottom here</div>
+        <div className={styles.footer}>Footer here</div>
       </div>
     </div>
   );
