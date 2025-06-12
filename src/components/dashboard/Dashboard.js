6a903522089c45bdd4423bab3d45e8df684aab94
Oskar Gester <gester.oskar@gmail.com>
2025-06-13 00:01:12 +0200

    header responsivness testing, name should be a image on top of background instead for better sizing

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import styles from "../../styles/layout/dashboardLayout.module.scss";
+import Header from "./header/headerComponent";
 //import TopBar from "./top/topBar";
 //import SideBar from "./side/sideComponent";
 //import AboutMe from "./aboutMe/aboutMeComponent";
@@ -7,7 +8,9 @@ export default async function Dashboard() {
   return (
     <div className={styles.wrapper}>
       <div className={styles.grid}>
-        <div className={styles.header}>Header here</div>
+        <div className={styles.header}>
+          <Header />
+        </div>
         <div className={styles.sidebar}>Sidebar here</div>
         <div className={styles.about_box}>About me here</div>
         <div className={styles.stats_box}>Stats section here</div>
--- src/components/dashboard/header/headerComponent.js
+++ src/components/dashboard/header/headerComponent.js
@@ -1,10 +1,5 @@
-import styles from "../../../styles/layout/top/topBarComponent.module.scss";
+import styles from "../../../styles/layout/header/headerComponent.module.scss";
 
-export default function TopBar() {
-  return (
-    <div className={styles.flexContainer}>
-      <div className={styles.circles}></div>
-      <div className={styles.myName}></div>
-    </div>
-  );
+export default function Header() {
+  return <div className={styles.flexContainer}></div>;
 }
