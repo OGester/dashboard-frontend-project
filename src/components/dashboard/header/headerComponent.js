22642cf7a86a42d5b37ea0e31b26254cc3567419
Oskar Gester <gester.oskar@gmail.com>
2025-07-06 10:31:34 +0200

    changes to header component. neds

--- src/components/dashboard/header/headerComponent.js
+++ src/components/dashboard/header/headerComponent.js
@@ -1,5 +1,6 @@
 import styles from "../../../styles/layout/header/headerComponent.module.scss";
 
 export default function Header() {
+  // add oskar gester text as an image on top of background
   return <div className={styles.flexContainer}></div>;
 }
